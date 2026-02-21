488febcf63751741c07bcff5afa7e11d79e405cf
Xaviel Terrero <xavieljoseterrerocuevas9@gmail.com>
2026-02-21 17:04:32 -0400

    Making the Active Links while scrolling

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +6,28 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {
+export class Navbar implements AfterViewInit {
+  activeSection = signal<string>('Home');
 
+  ngAfterViewInit(): void {
+    const sections = document.querySelectorAll('section');
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            this.activeSection.set(entry.target.id);
+          }
+        });
+      },
+      {
+        threshold: 0.6
+      }
+    );
+
+    sections.forEach(section => observer.observe(section));
+  }
 }
+
+
+
