4d70b7d70e2695a46bbb1da196f57086d9f90b4c
Xaviel Terrero <xavieljoseterrerocuevas9@gmail.com>
2026-03-17 23:56:04 -0400

    Fix contact form

--- api/contact.ts
+++ api/contact.ts
@@ -1,10 +1,7 @@
-export const config = {
-  runtime: 'nodejs'
-};
-
 import { Resend } from 'resend';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default async function handler(req: any, res: any) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
 
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
